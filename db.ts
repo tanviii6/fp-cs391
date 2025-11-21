cea6d975ac5e1abf18fac96af34340b5869a2cf9
Christian Gonzalez <chrisgo@bu.edu>
2025-11-21 14:04:59 -0500

    added mongodb + schemas

---
+++ db.ts
@@ -0,0 +1,53 @@
+import { MongoClient, Db, Collection } from "mongodb";
+import { User, Movie, Watched, Like, List, Collections } from "@/types/schemas";
+
+const MONGO_URI = process.env.MONGO_URI;
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI is not set");
+}
+
+const DB_NAME = "movieboxd";
+
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
+
+async function connect(): Promise<{ client: MongoClient; db: Db }> {
+  if (cachedDb && cachedClient) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = new MongoClient(MONGO_URI!);
+  await client.connect();
+
+  cachedClient = client;
+  cachedDb = client.db(DB_NAME);
+
+  return { client: cachedClient, db: cachedDb };
+}
+
+async function getCollection<Name extends keyof Collections>(
+  collectionName: Name,
+): Promise<Collections[Name]> {
+  const { db } = await connect();
+  return db.collection(collectionName) as Collections[Name];
+}
+
+export async function getUsersCollection(): Promise<Collection<User>> {
+  return getCollection("USERS_COLLECTION");
+}
+
+export async function getMoviesCollection(): Promise<Collection<Movie>> {
+  return getCollection("MOVIES_COLLECTION");
+}
+
+export async function getWatchedCollection(): Promise<Collection<Watched>> {
+  return getCollection("WATCHED_COLLECTION");
+}
+
+export async function getListCollection(): Promise<Collection<List>> {
+  return getCollection("LIST_COLLECTION");
+}
+
+export async function getLikesCollection(): Promise<Collection<Like>> {
+  return getCollection("LIKES_COLLECTION");
+}
---
+++ types/schemas.ts
@@ -0,0 +1,61 @@
+import { ObjectId, Document, Collection } from "mongodb";
+
+export interface User extends Document {
+  _id?: ObjectId;
+  avatar?: string;
+  bio?: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+}
+
+export interface Movie extends Document {
+  _id?: ObjectId;
+  tmdbId?: number;
+  title: string;
+  releaseYear?: number;
+  posterUrl?: string;
+  director?: string;
+  synopsis?: string;
+  runtimeMinutes?: number;
+  genres?: string[];
+  averageRating: number;
+  totalRatings: number;
+}
+
+export interface Watched extends Document {
+  _id?: ObjectId;
+  userId: ObjectId;
+  movieId: ObjectId;
+  watchedDate: Date;
+  rating?: number | null; // 0-5 or null
+  reviewText?: string;
+  isRewatch: boolean;
+  loggedAt: Date;
+}
+
+export interface Like extends Document {
+  _id?: ObjectId;
+  userId: ObjectId;
+  movieId: ObjectId;
+  createdAt: Date;
+}
+
+export interface List extends Document {
+  _id?: ObjectId;
+  userId: ObjectId;
+  title: string;
+  description?: string;
+  movies: ObjectId[];
+  isPublic: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Collections {
+  USERS_COLLECTION: Collection<User>;
+  MOVIES_COLLECTION: Collection<Movie>;
+  WATCHED_COLLECTION: Collection<Watched>;
+  LIST_COLLECTION: Collection<List>;
+  LIKES_COLLECTION: Collection<Like>;
+}
